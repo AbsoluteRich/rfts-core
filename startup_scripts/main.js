eb6d433b8adfb4e3135a628fe0bcb00b31c30231
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-09-06 22:54:48 +1000

    Fragments of new items
    These may or may not get used

---
+++ startup_scripts/main.js
@@ -0,0 +1,8 @@
+StartupEvents.registry("item", kubejs => {
+    // kubejs.create("chromatic_compound")
+    // kubejs.create("incomplete_electronic_mechanism").texture("part_advanced_mechanical_component_incomplete")
+    // kubejs.create("electronic_mechanism").texture("part_advanced_mechanical_component")
+    // kubejs.create("incomplete_circuit_board").texture("part_electronic_incomplete_green_circuit")
+    // kubejs.create("broken_circuit_board").texture("part_electronic_circuit_green_broken")
+    // kubejs.create("circuit_board").texture("part_electronic_circuit_integrated")
+})
