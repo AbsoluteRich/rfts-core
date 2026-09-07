a06749917dc932aecd3c660d03746871491f77ce
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-09-07 22:15:41 +1000

    Annihilate steel

---
+++ server_scripts/unification.js
@@ -0,0 +1,6 @@
+ServerEvents.recipes(event => {
+    event.remove("createnuclear:crafting/crafting/steel_ingot_from_decompacting")
+    event.remove("createnuclear:crafting/steel_ingot_from_decompacting")
+    event.remove("createnuclear:crafting/crafting/steel_ingot_from_compacting")
+    event.remove("createnuclear:mixing/steel")
+})
--- startup_scripts/main.js
+++ startup_scripts/main.js
@@ -1,8 +1,8 @@
-StartupEvents.registry("item", kubejs => {
-    // kubejs.create("chromatic_compound")
-    // kubejs.create("incomplete_electronic_mechanism").texture("part_advanced_mechanical_component_incomplete")
-    // kubejs.create("electronic_mechanism").texture("part_advanced_mechanical_component")
-    // kubejs.create("incomplete_circuit_board").texture("part_electronic_incomplete_green_circuit")
-    // kubejs.create("broken_circuit_board").texture("part_electronic_circuit_green_broken")
-    // kubejs.create("circuit_board").texture("part_electronic_circuit_integrated")
+StartupEvents.registry("item", event => {
+    // event.create("chromatic_compound")
+    // event.create("incomplete_electronic_mechanism").texture("part_advanced_mechanical_component_incomplete")
+    // event.create("electronic_mechanism").texture("part_advanced_mechanical_component")
+    // event.create("incomplete_circuit_board").texture("part_electronic_incomplete_green_circuit")
+    // event.create("broken_circuit_board").texture("part_electronic_circuit_green_broken")
+    // event.create("circuit_board").texture("part_electronic_circuit_integrated")
 })
