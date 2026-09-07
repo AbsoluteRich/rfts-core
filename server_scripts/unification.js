08bf6eeaad6869e9c51b8f77e66f2e4b9c5acff2
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-09-07 22:36:37 +1000

    Rose quartz

--- server_scripts/unification.js
+++ server_scripts/unification.js
@@ -1,5 +1,11 @@
 ServerEvents.recipes(event => {
-    // Todo: Convert BOP Rose Quartz into Create Rose Quartz
+    // Convert BOP Rose Quartz into Create Rose Quartz
+    event.shapeless(
+        "create:rose_quartz", // arg 1: output
+        [
+            "9x biomesoplenty:rose_quartz_block"
+        ]
+    )
 
     // Remove duplicate steel from Create Nuclear
     event.remove("createnuclear:crafting/crafting/steel_ingot_from_decompacting")
